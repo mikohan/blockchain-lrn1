f6538a32221f972d7bc45272fe5bc4ef6d376a0e
mikohan <angara99@gmail.com>
2022-02-13 15:22:30 +0300

    Deployment finished

--- deploy.js
+++ deploy.js
@@ -1,1 +1,23 @@
 // deploy code will go here
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+  "guard strategy island borrow muscle ready neck home alien faint someone bus",
+  "https://rinkeby.infura.io/v3/6bf0af44eba24c25b73f2fd526535efd"
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log("Attempting to deploy from accounts", accounts[0]);
+
+  result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .send({ gas: "1000000", from: accounts[0] });
+  console.log("Contract deployed to ", result.options.address);
+  provider.engine.stop();
+};
+deploy();
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -8,30 +8,36 @@ const { describe } = require("mocha");
 const Web3 = require("web3");
 
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require("../compile");
 
-class Car {
-  park() {
-    return "stopped";
-  }
-  drive() {
-    return "vroom";
-  }
-}
+let accounts;
+let inbox;
 
-let car;
-beforeEach(() => {
-  console.log("Run");
-  car = new Car();
+beforeEach(async () => {
+  // Get a list of al accounts
+
+  accounts = await web3.eth.getAccounts();
+  // Use one of those accounts to deploy
+  // the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there!"],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
 });
 
-describe("Car", () => {
-  it("can park", () => {
-    assert.equal(car.park(), "stopped");
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    assert.ok(inbox.options.address);
   });
-  it("can drive", () => {
-    assert.equal(car.drive(), "vroom");
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "Hi there!");
   });
-  it("cannot drive", () => {
-    assert.notEqual(car.drive(), "vroom ddd");
+  it("can change the message", async () => {
+    await inbox.methods.setMessage("bye").send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "bye");
   });
 });
